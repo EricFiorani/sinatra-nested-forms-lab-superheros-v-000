c681cd1b5ce0657fac509059d183d3507637b37a
Eric Fiorani <efiorani305@gmail.com>
2018-08-24 16:20:57 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,1 +1,16 @@
-fd
+class Team
+  attr_reader :name, :motto
+
+  @@team_array = []
+
+  def initialize(params)
+    @name = params[:name]
+    @motto = params[:motto]
+    @@team_array << self
+  end
+
+  def self.all
+    @@team_array
+  end
+
+end
